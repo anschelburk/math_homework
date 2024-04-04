3621fa5af6045d475ee7611a7e94b3e438d43586
anschelburk <anschel.burk@gmail.com>
2024-04-04 13:43:25 -0400

    Added first class to mathwork/models.py

--- mysite/mathwork/models.py
+++ mysite/mathwork/models.py
@@ -1,3 +1,13 @@
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
-# Create your models here.
+class math_assignment(models.Model):
+    student_name = models.CharField(max_length=255)
+    teacher_name = models.CharField(max_length=255, blank=True)
+    class_name = models.CharField(max_length=255, blank=True)
+    assignment_date = models.DateField(blank=True, null=True)
+    assignment_title = models.CharField(max_length=500, blank=False)
+    problems = JSONField
+
+    def __str__(self):
+        return f"Math assignment. Assignment Title: {self.assignment_title}. Student Name: {self.student_name}."
