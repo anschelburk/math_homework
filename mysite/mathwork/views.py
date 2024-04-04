9e0cd63bf270d9514805ffda8aaf9a2e62904cdb
anschelburk <anschel.burk@gmail.com>
2024-04-04 15:24:16 -0400

    Added template mysite/mathwork/templates/mathapp/editor.html, modified urls.py and views.py in mathwork folder accordingly.

--- mysite/mathwork/views.py
+++ mysite/mathwork/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return "Welcome to Math Homework - math work made easy!"
+
+def math_editor(request):
+    return render(request, 'mathapp/editor.html')
