3eb9879a356c4126d0d70213481640329f1a3bb4
anschelburk <anschel.burk@gmail.com>
2024-04-05 08:39:47 -0400

    Committing changes to editor and views.py

--- mysite/mathwork/views.py
+++ mysite/mathwork/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return "Welcome to Math Homework - math work made easy!"
+    return HttpResponse("Welcome to Math Homework - math work made easy!")
 
 def math_editor(request):
     return render(request, 'mathapp/editor.html')
