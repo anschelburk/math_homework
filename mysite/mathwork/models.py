96b0d7ea14f5812ba40f0472730280b39c702ffa
anschelburk <anschel.burk@gmail.com>
2024-04-05 08:25:37 -0400

    First migration

--- mysite/mathwork/models.py
+++ mysite/mathwork/models.py
@@ -1,4 +1,4 @@
-from django.contrib.postgres.fields import JSONField
+# from django.contrib.postgres.fields import JSONField
 from django.db import models
 
 class math_assignment(models.Model):
@@ -7,7 +7,7 @@ class math_assignment(models.Model):
     class_name = models.CharField(max_length=255, blank=True)
     assignment_date = models.DateField(blank=True, null=True)
     assignment_title = models.CharField(max_length=500, blank=False)
-    problems = JSONField
+    # problems = JSONField
 
     def __str__(self):
         return f"Math assignment. Assignment Title: {self.assignment_title}. Student Name: {self.student_name}."
