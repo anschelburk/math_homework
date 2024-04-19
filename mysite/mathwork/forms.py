3b420c906efc6df77e578685d3819838038e6bc4
Bob Belderbos <bobbelderbos@gmail.com>
2024-04-19 19:20:59 +0200

    bingo

--- mysite/mathwork/forms.py
+++ mysite/mathwork/forms.py
@@ -1,12 +1,10 @@
 from django import forms
 from .models import MathAssignment
-import mathfield
 
 class MathAssignmentForm(forms.ModelForm):
     class Meta:
         model = MathAssignment
         fields = "__all__"
         widgets = {
-            'problems': mathfield.MathFieldWidget,
-            # 'problems': forms.Textarea(attrs={'cols': 80, 'rows': 20}),
+            'problems': forms.Textarea(attrs={'cols': 80, 'rows': 20}),
         }
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mathwork.apps.MathworkConfig',
-    'mathfield'
 ]
 
 MIDDLEWARE = [
