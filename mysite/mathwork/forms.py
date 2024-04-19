923e63592e659b5d9666c6072f82860e92b34911
Anschel Burk <anschel.burk@gmail.com>
2024-04-19 08:49:25 -0700

    Added django-mathfield, modified text of submit button on assignmenta-form.html template.

--- mysite/mathwork/forms.py
+++ mysite/mathwork/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from .models import MathAssignment
+import mathfield
 
 class MathAssignmentForm(forms.ModelForm):
     class Meta:
         model = MathAssignment
         fields = "__all__"
         widgets = {
-            'problems': forms.Textarea(attrs={'cols': 80, 'rows': 20}),
+            'problems': mathfield.MathFieldWidget,
+            # 'problems': forms.Textarea(attrs={'cols': 80, 'rows': 20}),
         }
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mathwork.apps.MathworkConfig',
+    'mathfield'
 ]
 
 MIDDLEWARE = [
