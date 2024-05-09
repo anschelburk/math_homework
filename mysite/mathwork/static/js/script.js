3b18b97c82693694ae435d8d69c0f82374551bed
Bob Belderbos <bobbelderbos@gmail.com>
2024-05-09 10:15:16 +0200

    remove get cookie function

--- mysite/mathwork/static/js/script.js
+++ mysite/mathwork/static/js/script.js
@@ -82,20 +82,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
 
 // Update the background
 function updateCanvasBackground(selectedValue) {
