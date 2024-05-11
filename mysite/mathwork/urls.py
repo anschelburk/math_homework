751bccc6baa592c00c1a3dc504f19cfd914f41f4
Anschel Burk <anschel.burk@gmail.com>
2024-05-11 00:03:03 -0400

    Created new assignment_main template as central hub for widgets, with its own view. Create text_input.html page.

--- mysite/mathwork/urls.py
+++ mysite/mathwork/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("assignment/", views.assignment_view, name="math_assignmment"),
     path("mathkeyboard/", views.math_keyboard, name="math_keyboard"),
     path("drawing/", views.drawing_view, name="drawing"),
+    path("editassignment/", views.edit_assignment, name="edit_assignment"),
     path('save-drawing/', views.save_drawing, name='save_drawing'),
     path('load-drawing/<int:drawing_id>/', views.load_drawing, name='load_drawing'),
     path('list-drawings/', views.list_drawings, name='list_drawings'),
