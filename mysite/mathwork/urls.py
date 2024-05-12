c96a044c0f78d7d5664ca8f0a6e6725019d0c0fa
Anschel Burk <anschel.burk@gmail.com>
2024-05-11 20:03:18 -0400

    edit_assignment.html template now successfully loads the different templates from the dropdown list, but does not clear previous selections. Saving/retrieving from server not yet set up.

--- mysite/mathwork/urls.py
+++ mysite/mathwork/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
@@ -8,6 +7,7 @@ urlpatterns = [
     path("mathkeyboard/", views.math_keyboard, name="math_keyboard"),
     path("drawing/", views.drawing_view, name="drawing"),
     path("editassignment/", views.edit_assignment, name="edit_assignment"),
+    path("get-widget/", views.get_widget, name="get_widget"),
     path('save-drawing/', views.save_drawing, name='save_drawing'),
     path('load-drawing/<int:drawing_id>/', views.load_drawing, name='load_drawing'),
     path('list-drawings/', views.list_drawings, name='list_drawings'),
